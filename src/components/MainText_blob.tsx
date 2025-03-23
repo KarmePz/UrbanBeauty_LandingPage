7ccdb6050a63a3e3066edd17739181e7df5eff8c
KarmePz <baezjulicarme16@gmail.com>
2025-03-22 22:40:53 -0300

    add shadows to images

--- src/components/MainText_blob.tsx
+++ src/components/MainText_blob.tsx
@@ -10,9 +10,10 @@ const Maintext_Blob = () => {
     return (
         <>
         <div>
-            <img src="/images/woman_1.jpg" alt="mujer 1" className="absolute w-60 h-auto rounded-[40px] top-[12%] right-25"/>
-            <img src="/public/images/woman_2.jpg" alt="mujer 2"  className="absolute w-60 h-auto rounded-[40px] top-[30%] right-70"/>
-            <img src="/public/images/woman_3.jpg" alt="mujer 3"  className="absolute w-60 h-auto rounded-[40px] top-20"/>
+            <img src="/public/images/woman_3.jpg" alt="mujer 3"  className="absolute w-60 h-auto rounded-[40px] top-[12%] right-89  shadow-[#6300B5] shadow-2xl"/>
+            <img src="/images/woman_1.jpg" alt="mujer 1" className="absolute w-60 h-auto rounded-[40px] top-[15%] right-25  shadow-[#6300B5] shadow-2xl"/>
+            <img src="/public/images/woman_2.jpg" alt="mujer 2"  className="absolute w-58 h-auto rounded-[40px] top-[42%] right-55  shadow-[#6300B5] shadow-2xl"/>
+            
         </div>
         
         </>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () =>  {
     const button_style = "duration-350 hover:bg-fuchsia-950/50 p-4 rounded-2xl font-CormorantGaramond font-semibold text-[21px]"
     return (
         <>
-        <nav className="fixed z-1000 flex justify-between items-center pl-4 pr-25 pt-0 pb-0 bg-black text-white w-full " >
+        <nav className="fixed z-1000 flex justify-between items-center pl-25 pr-25 pt-0 pb-0 bg-black text-white w-full " >
             <img src="/images/Cosmetica_logo.jpg" alt="Urban Beauty" className="w-20 h-20 object-cover "></img>
             <ul className="flex space-x-6 ">
                 <li className={button_style}> <a href="#home"className="">Inicio</a></li>
